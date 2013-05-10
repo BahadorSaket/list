ec934d6d1b99ceac19fd9ea4a8112e622211449f
Alex Mingoia <talk@alexmingoia.com>
2013-05-09 23:04:04 -0700

    Update paging plugin to have previous/next buttons.

--- plugins/paging/list.paging.js
+++ plugins/paging/list.paging.js
@@ -5,8 +5,8 @@ List.prototype.plugins.paging = function(locals, options) {
         options = options || {};
         pagingList = new List(list.listContainer.id, {
             listClass: options.pagingClass || 'paging',
-            item: "<li><div class='page'></div></li>", // Have to contain something, can't set valueName at root element
-            valueNames: ['page', 'dotted'],
+            item: "<li><div class='page'></div></li>",
+            valueNames: ['page'],
             searchClass: 'nosearchclass',
             sortClass: 'nosortclass'
         });
@@ -20,65 +20,41 @@ List.prototype.plugins.paging = function(locals, options) {
             page = list.page,
             pages = Math.ceil(l / page),
             currentPage = Math.ceil((index / page)),
-            innerWindow = options.innerWindow || 2,
-            left = options.left || options.outerWindow || 0,
-            right = options.right || options.outerWindow || 0,
-            right = pages - right;
+            prev_class = (currentPage == 1) ? ' disabled' : '',
+            next_class = (currentPage == pages) ? ' disabled' : '';
 
         pagingList.clear();
-        for (var i = 1; i <= pages; i++) {
-            var className = (currentPage === i) ? "active" : "";
 
-            //console.log(i, left, right, currentPage, (currentPage - innerWindow), (currentPage + innerWindow));
+        var first = pagingList.add({
+            page: '<a class="button grey no-ajax' + prev_class + '" href="javascript:void(0);">&laquo; First</a>'
+        })[0].elm;
+        var prev = pagingList.add({
+            page: '<a class="button grey no-ajax' + prev_class + '" href="javascript:void(0);">&lsaquo; Previous</a>'
+        })[0].elm;
+        var pageNum = pagingList.add({
+            page: "<span>" + currentPage + " of " + pages + "</span>"
+        })[0].elm;
+        var next = pagingList.add({
+            page: '<a class="button grey no-ajax' + next_class + '" href="javascript:void(0);">Next &rsaquo;</a>'
+        })[0].elm;
+        var last = pagingList.add({
+            page: '<a class="button grey no-ajax' + next_class + '" href="javascript:void(0);">Last &raquo;</a>'
+        })[0].elm;
 
-            if (is.number(i, left, right, currentPage, innerWindow)) {
-                var item = pagingList.add({
-                    page: "<a class='"+className+"' href='javascript:function Z(){Z=\"\"}Z()'>"+(i)+"</a>",
-                    dotted: false
-                })[0];
-                addEvent(item.elm, i, page);
-            } else if (is.dotted(i, left, right, currentPage, innerWindow, pagingList.size())) {
-                pagingList.add({
-                    page: "...",
-                    dotted: true
-                });
-            }
+        if (!prev_class) {
+            addEvent(first, 1, page);
+            addEvent(prev, currentPage - 1, page);
         }
-    };
-
-    var is = {
-        number: function(i, left, right, currentPage, innerWindow) {
-           return this.left(i, left) || this.right(i, right) || this.innerWindow(i, currentPage, innerWindow);
-        },
-        left: function(i, left) {
-            return (i <= left);
-        },
-        right: function(i, right) {
-            return (i > right);
-        },
-        innerWindow: function(i, currentPage, innerWindow) {
-            return ( i >= (currentPage - innerWindow) && i <= (currentPage + innerWindow));
-        },
-        dotted: function(i, left, right, currentPage, innerWindow, currentPageItem) {
-            return this.dottedLeft(i, left, right, currentPage, innerWindow)
-            || (this.dottedRight(i, left, right, currentPage, innerWindow, currentPageItem));
-        },
-        dottedLeft: function(i, left, right, currentPage, innerWindow) {
-            return ((i == (left + 1)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right))
-        },
-        dottedRight: function(i, left, right, currentPage, innerWindow, currentPageItem) {
-            if (pagingList.items[currentPageItem-1].values().dotted) {
-                return false
-            } else {
-                return ((i == (right)) && !this.innerWindow(i, currentPage, innerWindow) && !this.right(i, right))
-            }
+        if (!next_class) {
+            addEvent(next, currentPage + 1, page);
+            addEvent(last, pages, page);
         }
     };
 
     var addEvent = function(elm, i, page) {
-       ListJsHelpers.addEvent(elm, 'click', function() {
-           list.show((i-1)*page + 1, page);
-       });
+         ListJsHelpers.addEvent(elm, 'click', function() {
+             list.show((i-1)*page + 1, page);
+         });
     };
 
     init();
